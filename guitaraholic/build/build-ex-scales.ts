b7136f3b6795a64d6c01b78a7237ac1f3bd0d79b
fananini <fumanayama@tech.softbank.co.jp>
2025-05-21 18:04:55 +0900

    #

--- guitaraholic/build/build-ex-scales.ts
+++ guitaraholic/build/build-ex-scales.ts
@@ -1,16 +1,17 @@
 import { scales } from "./music.ts";
 
 const build = (): void => {
-    const roots = ["C", "C#", "Db", "D", "D#", "Eb", "E"];
-    for (let scale of scales) {
-        if (scale.type === "modal" || scale.type === "altered") {
-            for (let root of roots) {
-                console.log(root + " " + scale.name);
-                const filename = scale.abbr + "-" + root.toLowerCase().replace("#", "s") + ".html"
-                console.log(filename)
-            }
-        }
+  const roots = ["C", "C#", "Db", "D", "D#", "Eb", "E"];
+  for (let scale of scales) {
+    if (scale.type === "modal" || scale.type === "altered") {
+      for (let root of roots) {
+        console.log(root + " " + scale.name);
+        const filename =
+          scale.abbr + "-" + root.toLowerCase().replace("#", "s") + ".html";
+        console.log(filename);
+      }
     }
-}
+  }
+};
 
 build();
