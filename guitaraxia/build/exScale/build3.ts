d0d4e27935939745d8e4e37995d8558e259d82c6
fananini <fumanayama@tech.softbank.co.jp>
2025-06-06 18:15:06 +0900

    #

--- guitaraxia/build/exScale/build3.ts
+++ guitaraxia/build/exScale/build3.ts
@@ -5,18 +5,30 @@ import { NamedValue } from "../namedValue.ts";
 import { fileRead, fileWrite } from "../file.ts";
 import { embed } from "../embed.ts";
 
+const rowTemplate =
+  "----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+-";
+
+function makeRow(row: string, fret: number, degree: number): string {
+  if (0 <= fret && fret <= 15) {
+    
+  }
+  return row;
+}
+
+)
 function build3(): void {
   const data = fileRead("./scales.json");
   let scales = JSON.parse(data);
 
   for (let scale of scales) {
     const title = scale.key + " " + scale.name;
-    const fileName = scale.abbr + "-" + scale.key.toLowerCase().replace("#", "s") + ".html";
+    const fileName =
+      scale.abbr + "-" + scale.key.toLowerCase().replace("#", "s") + ".html";
     console.log(fileName);
     const htmlTemplate = fileRead("./htmlTemplate.html");
     const embeddings: NamedValue[] = [
       new NamedValue("title", title),
-      new NamedValue("main", "***")
+      new NamedValue("main", "***"),
     ];
     const htmlModified = embed(htmlTemplate, embeddings);
     fileWrite("../results/exercises/scales/" + fileName, htmlModified);
